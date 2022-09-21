0e16d537768fd689042a1250ca727df4783e8ab0
Victor <victorreis222@gmail.com>
2022-09-21 13:59:39 -0300

    Requisitos 01, 02, 03 e 04, com erro de lint

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>{ headline }</h2>
+    );
+  }
+}
+
+export default Title;
+
+Title.propTypes = {
+  Title: propTypes.shape({
+    headline: propTypes.string,
+  }).isRequired,
+};
